b20acb9e4d6e420266091a8f59fc1840e527b00f
Emily Ebling <eblingemily17@gmail.com>
2020-09-06 19:54:38 -0400

    Updates

---
+++ precache-manifest.fbf915860168d7710c90dbaf0849449c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae4abbf229768737ebcb5d3e1ba536c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63b6ac85af92c9641861",
+    "url": "/static/css/main.95d0826c.chunk.css"
+  },
+  {
+    "revision": "16503dc467d591bfb225",
+    "url": "/static/js/2.56af3e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56af3e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b6ac85af92c9641861",
+    "url": "/static/js/main.33ff696a.chunk.js"
+  },
+  {
+    "revision": "e673e0a2286a0cd6a825",
+    "url": "/static/js/runtime-main.b8afb425.js"
+  }
+]);
