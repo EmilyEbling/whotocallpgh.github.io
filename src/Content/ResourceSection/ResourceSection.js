776f19156dc79bfc8ce4ed76872f2afb721e4f56
AaronRiegel <riegelaaron@gmail.com>
2020-09-08 21:03:14 -0400

    addition to filtering

--- src/Content/ResourceSection/ResourceSection.js
+++ src/Content/ResourceSection/ResourceSection.js
@@ -11,7 +11,7 @@ class ResourceSection extends Component {
         super();
         this.state = {
             search: '',
-            selectedOption: null
+            selectedOption: {}
         };
     }
 
@@ -22,18 +22,57 @@ class ResourceSection extends Component {
 
     handleChange = (selectedOption) => {
         this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
+        //console.log(`Option selected: ${selectedOption}`);
     }
 
-    render() {
+    filtering() {
+        console.log(`num of checks: ${JSON.stringify(this.state.selectedOption.length)}`);
+        console.log(Object.entries(this.state.selectedOption).length);
+
+        if (this.state.selectedOption == null || Object.entries(this.state.selectedOption).length === 0) {
+            console.log(`empty`);
+            return Resources;
+        }
+
+        //console.log(`${Object.entries(this.state.selectedOption)}`);
+
+        console.log(`filter: ${JSON.stringify(Object(this.state.selectedOption.value))}`);
+        console.log(`filter includes Immediate: ${this.state.selectedOption.value === "Immediate"}`);
+
+        var filtered = Resources.filter(data =>
+            data.tags.some(tag => this.state.selectedOption.value === tag)).map(data => {
+                return (
+                    <div key={data.name}>
+                        <Resource
+                            name={data.name}
+                            phone_link={data.phone_link}
+                            formatted_phone={data.formatted_phone}
+                            website={data.website}
+                            description={data.description}
+                            tags={data.tags}>
+                        </Resource>
+                    </div>
+                )
+            })
+
+        console.log(Object.entries(filtered).length);
+        return filtered;
+    }
+
+    search(filtered) {
+        //console.log(filtered)
         var result = Resources.filter((data) => {
-            if (this.state.search == null)
-                return data
-            else if (data.description.toLowerCase().includes(this.state.search.toLowerCase())
+            if (data.description.toLowerCase().includes(this.state.search.toLowerCase())
                 || data.name.toLowerCase().includes(this.state.search.toLowerCase())
-                || data.tags.some(tag => tag.toLowerCase().includes(this.state.search.toLowerCase()))) {
-                return data
+                || data.tags.some(tag => tag.toLowerCase().includes(this.state.search.toLowerCase()))
+            ) {
+                return data;
             }
+            else if (this.state.search == null)
+                return data;
+            else
+                return null;
+
         }).map(data => {
             return (
                 <div key={data.name}>
@@ -49,8 +88,18 @@ class ResourceSection extends Component {
             )
         })
 
-        if (Object.keys(result).length === 0 && this.state.search !== null) {
-            result = <h3>No Results Found</h3>
+        return result
+    }
+
+    render() {
+
+        var x = this.filtering()
+        console.log(`fek: ${x}`)
+
+        let display = this.search(Object.entries(x));
+
+        if (Object.keys(display).length === 0 && this.state.search !== null) {
+            display = <h3>No Results Found</h3>
         }
 
         return (
@@ -68,7 +117,7 @@ class ResourceSection extends Component {
                     </div>
                 </div>
                 <div>
-                    {result}
+                    {display}
                 </div>
             </div>
         )
